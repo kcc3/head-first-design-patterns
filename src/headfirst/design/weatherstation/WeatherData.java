f1e2d7355c4fecc862f56d438639ad9968930572
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2021-03-23 23:19:44 -0700

    Ch 2 - using Java builtin observer class

--- src/headfirst/design/weatherstation/WeatherData.java
+++ src/headfirst/design/weatherstation/WeatherData.java
@@ -1,35 +1,16 @@
 package weatherstation;
 
-import java.util.ArrayList;
+import java.util.Observable;
 
-public class WeatherData implements Subject {
-	private ArrayList<Observer> observers;
+public class WeatherData extends Observable {
 	private float temperature;
 	private float humidity;
 	private float pressure;
 	
-	public WeatherData() {
-		observers = new ArrayList<Observer>();
-	}
-	
-	public void registerObserver(Observer o) {
-		observers.add(o);
-	}
-	
-	public void removeObserver(Observer o) {
-		int i = observers.indexOf(o);
-		if (i  >= 0) {
-			observers.remove(i);
-		}
-	}
-	
-	public void notifyObservers() {
-		for (Observer observer : observers) {
-			observer.update(temperature,  humidity,  pressure);
-		}
-	}
+	public WeatherData() { }
 	
 	public void measurementsChanged() {
+		setChanged();
 		notifyObservers();
 	}
 	
@@ -40,5 +21,15 @@ public class WeatherData implements Subject {
 		measurementsChanged();
 	}
 	
-	// other WeatherData methods here
+	public float getTemperature() {
+		return temperature;
+	}
+	
+	public float getHumidity() {
+		return humidity;
+	}
+	
+	public float getPressure() {
+		return pressure;
+	}
 }
