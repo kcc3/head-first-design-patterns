c76fc085c7d6a75ded65d7d5e5bdbe39dbe45075
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2021-05-13 02:05:33 -0700

    Singleton
    getInstance call to make boiler into a singleton

--- src/headfirst/design/singleton/chocolate/ChocolateBoiler.java
+++ src/headfirst/design/singleton/chocolate/ChocolateBoiler.java
@@ -3,10 +3,20 @@ package singleton.chocolate;
 public class ChocolateBoiler {
 	private boolean empty;
 	private boolean boiled;
+	private static ChocolateBoiler uniqueInstance;
 	
 	public ChocolateBoiler() {
 		empty = true;
 		boiled = false;
+	}
+	
+	public static ChocolateBoiler getInstance() {
+		if (uniqueInstance == null) {
+			System.out.println("Creating unique instance of Chocolate Boiler");
+			uniqueInstance = new ChocolateBoiler();
+		}
+		System.out.println("Returning instance of Chocolate Boiler");
+		return uniqueInstance;
 	}
 	
 	public void fill() {
