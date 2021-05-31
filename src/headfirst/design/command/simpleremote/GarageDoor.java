f2339c5be326df127f9aaf3c0ba0079eed84f52b
Kevin Chang <kevin.chaohwa.chang@gmail.com>
2021-05-31 02:08:46 -0700

    Ch 6 - Command Pattern
    Initial exercises and code for command pattern

---
+++ src/headfirst/design/command/simpleremote/GarageDoor.java
@@ -0,0 +1,25 @@
+package command.simpleremote;
+
+public class GarageDoor {
+	public GarageDoor() {}
+	
+	public void up() {
+		System.out.println("Garage Door Up");
+	}
+	
+	public void down() {
+		System.out.println("Garage Door Down");
+	}
+	
+	public void stop() {
+		System.out.println("Garage Door Stop");
+	}
+	
+	public void lightOn() {
+		System.out.println("Garage Door Light On");
+	}
+	
+	public void lightOff() {
+		System.out.println("Garage Door Light Off");
+	}
+}
